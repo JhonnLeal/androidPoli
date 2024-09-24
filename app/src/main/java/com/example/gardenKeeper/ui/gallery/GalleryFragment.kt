6d4d0b1bae52b527f4026f50dfd73316568678b7
fentyr88 <juancrv345@gmail.com>
2024-09-24 00:18:13 -0500

    feature(HU2): Implement search functionality and display images in a grid layout.

--- app/src/main/java/com/example/gardenKeeper/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/gardenKeeper/ui/gallery/GalleryFragment.kt
@@ -4,34 +4,41 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.gardenKeeper.ui.gallery.GalleryAdapter
+import com.example.gardenkeeper.R
 import com.example.gardenkeeper.databinding.FragmentGalleryBinding
 
 class GalleryFragment : Fragment() {
-
     private var _binding: FragmentGalleryBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var galleryViewModel: GalleryViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
-
+        galleryViewModel = ViewModelProvider(this).get(GalleryViewModel::class.java)
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        // Lista de imágenes (puedes añadir tantas como desees)
+        val images = listOf(
+            R.drawable.add, R.drawable.add, R.drawable.add,
+            R.drawable.add, R.drawable.add, R.drawable.add,
+            R.drawable.add, R.drawable.add, R.drawable.add,
+            R.drawable.add, R.drawable.add, R.drawable.add
+        )
+
+        // Configuración del RecyclerView con GridLayoutManager
+        val gridLayoutManager = GridLayoutManager(context, 3) // 3 columnas
+        binding.galleryRecyclerView.layoutManager = gridLayoutManager
+        binding.galleryRecyclerView.adapter = GalleryAdapter(images)
+
         return root
     }
 
