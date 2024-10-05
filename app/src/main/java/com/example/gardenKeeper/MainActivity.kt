a8f2b3a5b799427552b683141b6e86fe454eaf75
Ronald Santos <ronsan12@outlook.com>
2024-10-05 15:16:53 -0500

    androidManifest,vistaweb, Main activity

--- app/src/main/java/com/example/gardenKeeper/MainActivity.kt
+++ app/src/main/java/com/example/gardenKeeper/MainActivity.kt
@@ -2,21 +2,26 @@ package com.example.gardenkeeper
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.gardenkeeper.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import com.example.gardenkeeper.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var webView: WebView
+    private lateinit var searchView: SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,19 +39,50 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow , R.id.nav_recordatorios, R.id.nav_plant_profile
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_recordatorios, R.id.nav_plant_profile
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // Inicializa el WebView y el SearchView
+        webView = findViewById(R.id.webView)
+        searchView = findViewById(R.id.searchView)
+
+        // Configura el WebView
+        webView.webViewClient = WebViewClient() // Permite que el WebView maneje los enlaces
+        webView.settings.javaScriptEnabled = true // Habilita JavaScript si es necesario
+
+        // Captura el evento de búsqueda
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                loadUrl(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+    }
+
+    private fun loadUrl(query: String?) {
+        // Agrega "http://" si la URL no comienza con "http"
+        val url = if (query != null && !query.startsWith("http")) {
+            "http://$query"
+        } else {
+            query
+        }
+        // Asegúrate de que url no sea nulo antes de cargarlo
+        url?.let {
+            webView.loadUrl(it)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
