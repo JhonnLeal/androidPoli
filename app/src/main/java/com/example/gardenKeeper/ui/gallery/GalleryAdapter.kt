c6be56d0e0c0621ca3fa8d0a583e51471a6c14f4
fentyr88 <juancrv345@gmail.com>
2024-10-07 00:27:26 -0500

    feature(HU): Add functionality to change profile image and update text labels based on user input - Implemented a TextWatcher for the SearchView to detect when 'Flor de lis' is entered. - Created a method to change the profile image and update the text labels for location, needs, and observations when the profile image is clicked or when 'Flor de lis' is detected.

--- app/src/main/java/com/example/gardenKeeper/ui/gallery/GalleryAdapter.kt
+++ app/src/main/java/com/example/gardenKeeper/ui/gallery/GalleryAdapter.kt
@@ -1,5 +1,6 @@
-package com.example.gardenKeeper.ui.gallery
+package com.example.gardenkeeper.ui.gallery
 
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,7 +8,7 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gardenkeeper.R
 
-class GalleryAdapter(private val images: List<Int>) : RecyclerView.Adapter<GalleryAdapter.ViewHolder>() {
+class GalleryAdapter(private val images: List<Any>) : RecyclerView.Adapter<GalleryAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val imageView: ImageView = view.findViewById(R.id.item_image)
@@ -20,9 +21,13 @@ class GalleryAdapter(private val images: List<Int>) : RecyclerView.Adapter<Galle
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.imageView.setImageResource(images[position])
+        val image = images[position]
+        if (image is Uri) {
+            holder.imageView.setImageURI(image)
+        } else if (image is Int) {
+            holder.imageView.setImageResource(image)
+        }
     }
 
     override fun getItemCount() = images.size
 }
-
