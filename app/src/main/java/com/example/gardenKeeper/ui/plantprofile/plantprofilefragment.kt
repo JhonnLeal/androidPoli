8bc094e783426adbbabb4dd9fdb3889bf23d6ffd
Ing.Wmateus <wa.mb@outlook.com>
2024-10-07 23:14:21 -0300

    correccion en plantprofileframent y ajustes en el layout del perfil de las plantas

--- app/src/main/java/com/example/gardenKeeper/ui/plantprofile/plantprofilefragment.kt
+++ app/src/main/java/com/example/gardenKeeper/ui/plantprofile/plantprofilefragment.kt
@@ -1,32 +1,49 @@
-package com.example.gardenkeeper.ui.plantprofile
+package com.example.gardenKeeper.ui.plantprofile
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.gardenKepper.ui.plantprofile.PlantProfileViewModel
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.gardenkeeper.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class Plantprofilefragment : Fragment() {
+class PlantProfileFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = Plantprofilefragment()
-    }
+    private lateinit var plantProfileViewModel: PlantProfileViewModel
+    private lateinit var plantAdapter: PlantAdapter
 
-    private val viewModel: PlantProfileViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_plant_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_plant_profiles, container, false)
+
+        // Inicializar ViewModel
+        plantProfileViewModel = ViewModelProvider(requireActivity())[PlantProfileViewModel::class.java]
+        plantAdapter = PlantAdapter(mutableListOf()) // Inicializar con lista vacía
+
+        val recyclerView: RecyclerView = view.findViewById(R.id.recycler_view_plant_profiles)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = plantAdapter
+
+        // Observa el LiveData para actualizar la lista cuando cambien los datos
+        plantProfileViewModel.plantProfiles.observe(viewLifecycleOwner) { plantProfiles ->
+            plantAdapter.updatePlantProfiles(plantProfiles)
+        }
+
+
+        val fabAddPlant = view.findViewById<FloatingActionButton>(R.id.fab_add_plant)
+        fabAddPlant.setOnClickListener {
+            // Iniciar la navegación hacia AddPlantFragment usando el NavController
+            findNavController().navigate(R.id.addPlantFragment)
+        }
+
+        return view
     }
 }
